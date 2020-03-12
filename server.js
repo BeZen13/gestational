f3cc40d41f507c07bc6beffbb2389834a46f0bc7
Samuel Joseph Betzen <samuelbetzen@gmail.com>
2020-03-12 17:16:57 -0600

    server so far built

---
+++ server.js
@@ -0,0 +1,43 @@
+const express = require('express')
+const app = express()
+require('dotenv').config()
+const morgan = require('morgan')
+const mongoose = require('mongoose')
+const expressJwt = require('express-jwt')
+
+//middleware
+
+app.use(express.json())
+app.use(morgan('dev'))
+
+mongoose.connect(
+    'mongoose://localhost:27017/gestational',
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+        useFindAndModify: false
+    },
+    () => console.log('You are now connected to the Gestational Diabetes DB')
+)
+
+//downloads?
+
+//endpoints
+app.use('/auth', require('./routes/authRouter'))
+app.use('api', expressJwt({ secret: process.env.SECRET })) //req.user
+
+//more to come
+
+app.use((err, req, res,  next) => {
+    console.log(err)
+    if(err.name === "UnauthorizedError"){
+        res.status(err.status)
+    }
+    return res.send({ errMsg: err.message })
+})
+
+//listen
+app.listen(8888, () => {
+    console.log('The Gestational Diabetes Servers is Poppin! Local port 8888')
+})
