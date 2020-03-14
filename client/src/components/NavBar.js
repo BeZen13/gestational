4385bd21476af30e6e5f5c595610345ca2fdb1af
Samuel Joseph Betzen <samuelbetzen@gmail.com>
2020-03-13 20:54:21 -0600

    NavBar Stuff

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -2,4 +2,47 @@ import React from 'react'
 import styled from 'styled-components'
 
 import bloody from '../assets/BloodLove.jpg'
-import NavvyLinks from '../components/NavvyLi'
+import NavvyLinks from '../components/NavvyLinks.js'
+import { NLink } from './styledcomponents/NLink.js'
+
+const NavBarContainer = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    width: 100%;
+    height: 130px;
+    position: relative;
+    background-color: #286653;
+    top: 0;
+    left: 0;
+    right: 0;
+`
+
+const Header = styled.div`
+    margin: 24px 0 24px 5%;
+`
+
+const HeaderLogoLink = styled(NLink)`
+    :hover {
+        border: none;
+    }
+`
+
+const HeaderLogo = styled.img`
+    max-height: 75px;
+    width: auto;
+    height: auto;
+`
+
+export default function NavBar(props){
+    return(
+        <NavBarContainer>
+            <Header>
+                <HeaderLogoLink to="/">
+                    <HeaderLogo src={bloody} alt="One Blood Mon!" />
+                </HeaderLogoLink>
+            </Header>
+            <NavvyLinks />
+        </NavBarContainer>
+    )
+}
