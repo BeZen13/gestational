4ac8841be6934983324c11fa9b65374ee62ec73f
Samuel Joseph Betzen <samuelbetzen@gmail.com>
2020-03-15 11:22:40 -0600

    navvyLinks

--- client/src/components/NavvyLinks.js
+++ client/src/components/NavvyLinks.js
@@ -1,2 +1,32 @@
 import React, { useContext } from 'react'
 import styled from "styled-components"
+import { withRouter } from 'react-router-dom'
+import { UserContext } from '../context/UserProvider.js'
+
+import { NLink } from './styledComponents/NLink.js'
+
+const LinkWrap  = styled.div`
+    display: flex;
+    justify-content: space-around;
+    width: 430px;
+    height: 50px;
+    padding: 10px;
+    margin: 0 5% 0 0;
+    align-items: center;
+`
+
+export default withRouter(NavBarLinks(props)){
+    const { token, logout } = useContext(UserContext)
+
+    return(
+        <LinkWrap>
+            <NLink to="/Home">Home</NLink>
+            <NLink to="/Numbers">Levels DB</NLink>
+            { token ? (
+                <NLink to="/" onClick={ logout }>
+                    Logout
+                </NLink>
+            ) : null }
+        </LinkWrap>
+    )
+}
