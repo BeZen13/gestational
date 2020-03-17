1cd2ee2a0f9b91a492bfd8f11fb722abb49737e5
Samuel Joseph Betzen <samuelbetzen@gmail.com>
2020-03-16 18:38:18 -0600

    starting number tracker arrangement component

--- client/src/components/NumbersTracker.js
+++ client/src/components/NumbersTracker.js
@@ -1,2 +1,6 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import './NumbersForm.js'
+import './NumberList.js'
 import styled from 'styled-components'
+
+//data layout component
