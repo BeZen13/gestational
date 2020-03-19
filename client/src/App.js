7ac79ca7f71a812f198d0eaa2a789cf9d78f33b1
Samuel Joseph Betzen <samuelbetzen@gmail.com>
2020-03-18 20:21:53 -0600

    added app

--- client/src/App.js
+++ client/src/App.js
@@ -1,3 +1,35 @@
 import React, { useContext } from 'React'
 import NavBar from './NavBar'
 import { Switch, Route, Redirect } from 'react-router-dom'
+import Auth from './components/Auth.js'
+import ProtectedRoute from './components/ProtectedRoute.js'
+import NumbersTracker from './components/NumbersTracker.js'
+import Home from './components/Home.js'
+
+export default function App(){
+    const { token, logout } = useContext(UserContext)
+
+    return(
+        <div className="app">
+            { token && <NavBar logout={logout}/> }
+            <Switch>
+                <Route
+                    exact path="/"
+                    render={() => token ? <Redirect to="/Home" /> : <Auth/>}
+                />
+                <ProtectedRoute
+                    path="/NumbersTracker"
+                    component={NumbersTracker}
+                    redirectTo="/"
+                    token={token}
+                />
+                <ProtectedRoute
+                    path="Home"
+                    component={Home}
+                    redirectTo="/"
+                    token={token}
+                />     
+            </Switch>
+        </div>
+    )
+}
