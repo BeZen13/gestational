813c6c7ffeefc4022dba60cd37589c6f83e4a27d
Samuel Joseph Betzen <samuelbetzen@gmail.com>
2020-03-23 09:15:54 -0600

    redo auth

--- client/src/components/Auth.js
+++ client/src/components/Auth.js
@@ -1,1 +1,75 @@
 import React, {useState, useContext} from 'react'
+import AuthForm from './AuthForm.js'
+import { UserContext } from '../context/UserProvider.js'
+import styled from 'styled-components'
+
+
+const initInputs = { username: "", password: "" }
+
+
+const AuthFormWrap = styled.div`
+
+`
+
+const WelcomeMessage = styled.div`
+
+`
+
+export default function Auth(){
+    const [ inputs, setInputs ] = useState(initInputs)
+    const [ toggle, setToggle ] = useState(false)
+
+    const { signup, login, errMsg, resetAuthErr } = useContext(UserContext)
+
+    function handleChange(e){
+        const {name, value} = e.target
+        setInputs(prevInputs => ({
+            ...prevInputs,
+            [name]: value
+        }))
+    }
+
+    function handleSignup(e){
+        e.preventDefault()
+        signup(inputs)
+    }
+
+    function handleLogin(e){
+        e.preventDefault()
+        login(inputs)
+    }
+
+    function toggleForm(){
+        setToggle(prev => !prev)
+        resetAuthErr()
+    }
+
+    return(
+        <div className="auth-container">
+            <h1></h1>
+            { !toggle ?
+                <>
+                    <AuthForm
+                        handleChange={ handleChange }
+                        handleSubmit={ handleSignup }
+                        inputs={inputs}
+                        btnText="Sign Up"
+                        errMsg={errMsg}
+                    />
+                    <p onClick={toggleForm}>Already a Member?</p>
+                </>
+            :
+                <>
+                    <AuthForm 
+                        handleChange={ handleChange }
+                        handleSubmit={ handleLogin }
+                        inputs={inputs}
+                        btnText="Login"
+                        errMsg={errMsg}
+                    />
+                    <p onClick={toggleForm}>Not a Member Yet?</p> 
+                </>           
+            }
+        </div>
+    )
+}
