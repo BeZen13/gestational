f3a3e98b2b3c5036cbff3b56bbe2717862ce5518
Samuel Joseph Betzen <samuelbetzen@gmail.com>
2020-03-28 09:35:59 -0600

    fixed responsiveness for homepage png

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -6,8 +6,8 @@ import gest from '../assets/gest.PNG'
 
 const IMGWrap = styled.img`
     display: flex;
-    height: 400px;
-    width 575px;
+    height: auto;
+    width auto;
     margin: 0 10px 10px 30px;
     margin-left: 25%;
     margin-top: 40px;  
