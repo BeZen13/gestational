5b48fb355b23d73a843111e3778c4bda93d3b9ff
Samuel Joseph Betzen <samuelbetzen@gmail.com>
2020-04-07 22:16:12 -0600

    Recipes

--- client/src/components/Recipes.js
+++ client/src/components/Recipes.js
@@ -1,6 +1,9 @@
 import React, { useState } from 'react'
 import axios from 'axios'
-
+import styled from 'styled-components'
 
 //axios call somewhere in here
 
+export default function Recipes(){
+    
+}
