bb55980101456c306dc9397cf36a7294cc345c23
Samuel Joseph Betzen <samuelbetzen@gmail.com>
2020-04-09 16:12:04 -0600

    more stuff

--- client/src/components/NumbersTracker.js
+++ client/src/components/NumbersTracker.js
@@ -8,7 +8,7 @@ import styled from 'styled-components'
 
 const NumbersTrackerWrapper = styled.div`
     display: block;
-    width: auto;
+    width: 100%;
     height: auto;
     margin: 10px 10px 10px 30px;
     margin-left: auto;
