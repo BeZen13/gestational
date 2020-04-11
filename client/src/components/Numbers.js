84060cb2bd9b22dfa3816a61919608ccad73af09
Samuel Joseph Betzen <samuelbetzen@gmail.com>
2020-04-10 20:48:20 -0600

    one thing fixed now multiple things broken

--- client/src/components/Numbers.js
+++ client/src/components/Numbers.js
@@ -1,6 +1,6 @@
 import React, { useContext } from 'react'
 import styled from 'styled-components'
-import UserContext from '../context/UserProvider.js'
+import { UserContext } from '../context/UserProvider.js'
 import { VictoryLine, VictoryTheme, VictoryChart, VictoryAxis } from 'victory'
 
 
